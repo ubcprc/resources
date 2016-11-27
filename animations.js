15c60ce5bbad6e9dc05d8a64d8aff862cd057330
Ben <ben.clark456@gmail.com>
2016-11-27 08:40:59 -0800

    Edited stuff...

---
+++ animations.js
@@ -0,0 +1,13 @@
+/**
+ *  Some javascript to control the little animations on the first ever
+ *   paper-free bulletin board :)
+ *
+ * Author: Ben Clark - 2016
+ *
+ **/
+
+/** Stupid test function **/
+document.addEventListener('click', function(){
+    alert("it's connected!");
+});
+
